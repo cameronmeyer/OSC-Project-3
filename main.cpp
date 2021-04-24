eec0c16afe948cc33859b133927a3574d1203c5c
Cameron Meyer <cdm180003@cslinux1.utdallas.edu>
2021-04-24 17:23:48 -0500

    FCFS Scheduling Implemented

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@
 #include <vector>
 #include <stdio.h>
 #include <iostream>
-#include <sys/wait.h>
-#include <sys/types.h>
 #include <stdexcept>
 #include <exception>
 #include <unistd.h>
@@ -19,25 +17,9 @@
 #include <tuple>
 using namespace std;
 
-const string whitespace = "\n\t\r\v\f";      // Whitespace characters for parsing the input file
+const string whitespace = "\n\t\r\v\f"; // Whitespace characters for parsing the input file
 
 vector<tuple<string, int, int>> jobs; 
-
-vector<int> getIntFromString(string s)
-{
-    string number = s.substr(0, s.find(whitespace)); // Only use the beginning of the line until whitespace is found
-    try
-    {
-        // Convert string to int
-        std::string::size_type sz;
-        int result = stoi(number, &sz);
-        return vector<int>{1, result}; // Index 0 in this vector indicates a success
-    }
-    catch(...)
-    {
-        return vector<int>{-1, 0}; // Index 0 in this vector indicates a failure for int conversion
-    }
-}
 
 void populateJobs(string fileName)
 {
@@ -57,25 +39,19 @@ void populateJobs(string fileName)
 
             for(char c : input)
             {
-                //cout << "1 -- c: " << c << endl;
                 if(count < 3) // Each line should have 3 elements we need to keep track of
                 {
-                    //cout << "2" << endl;
                     if(whitespace.find(c) != string::npos) // Check if the current char is a whitespace
                     {
-                        //cout << "3" << endl;
-                        if(entry != "") // If entry contains something, this whitespace is the end of the entry
+                        if(entry != "") // If entry contains something, this whitespace marks the end of the entry
                         {
-                            //cout << "4" << endl;
                             line.push_back(entry);
                             count++;
                             entry = "";
                         }
                     }
-                    //else if(input.substr(index, input.size()).find(c) == input.size() - 1)  // THIS IS ALWAYS GOING TO FIND IT AT THE BEGINNING OF THE SUBSTRING, WE JUST NEED THE LAST OCCURRENCE
-                    else if(input.find_last_of(c) == input.size() - 1)
+                    else if(index == input.size() - 1 && input.find_last_of(c) == input.size() - 1) // If the string ends with a non-whitespace character
                     {
-                        //cout << "7" << endl;
                         entry = entry + c;
                         line.push_back(entry);
                         count++;
@@ -83,41 +59,25 @@ void populateJobs(string fileName)
                     }
                     else // Add non-whitespace characters to the entry
                     {
-                        //cout << "5" << endl;
                         entry = entry + c;
                     }
-
-                    //cout << "c at end of string: " << input.find(c) << endl;
                 }
-                //cout << "input.substr(index, input.size()).find(c) " << input.substr(index, input.size()).find(c) << endl;
                 index++;
             }
 
-            //cout << "line size: " << line.size() << endl;
             if(line.size() == 3)
             {
-                //cout << "6" << endl;
                 tuple <string, int, int> job;
                 job = make_tuple(line[0], stoi(line[1]), stoi(line[2]));
-                //job = make_tuple(line[0], getIntFromString(line[1]), getIntFromString(line[2]));
                 jobs.push_back(job);
             }
-
-            //if(input.substr(0, 1) == ".")
-            //{
-            //    string errorMessage = "Error: Invalid memory write at address: " + to_string(tempAddress);
-            //    throw out_of_range(errorMessage);
-            //    exit(-1);
-            //}
-            //cout << "count: " << count << endl;
         }
 
-        cout << "jobs size: " << jobs.size() << endl;
+        //cout << "jobs size: " << jobs.size() << endl;
 
         for(auto job : jobs)
         {
-            cout << "job num elements: " << tuple_size<decltype(job)>::value << endl;
-            //printf(get<0>(job), "\n");
+            //cout << "job num elements: " << tuple_size<decltype(job)>::value << endl;
             cout << get<0>(job) << " " << get<1>(job) << " " << get<2>(job) << endl;
         }
 
@@ -131,11 +91,62 @@ void populateJobs(string fileName)
     }
 }
 
+void printSchedule(string algorithm, vector<string> schedule)
+{
+    cout << algorithm << endl << endl;
+    
+    int index = 0;
+    if(jobs.size() == schedule.size())
+    {
+        for(auto job : jobs)
+        {
+            cout << get<0>(job) << " " << schedule[index] << endl;
+            index++;
+        }
+    }
+}
+
+void fcfs()
+{
+    vector<string> schedule;
+    string running = "";
+
+    for(auto job : jobs) { schedule.push_back(""); }
+
+    int completedJobs = 0;
+    while(true)
+    {
+        int jobIndex = 0;
+
+        int runDuration = get<2>(jobs[completedJobs]);
+        running = get<0>(jobs[completedJobs]);
+
+        for(auto job : jobs)
+        {
+            for(int i = 0; i < runDuration; i++)
+            {
+                if(running == get<0>(job)) { schedule[jobIndex] += "X"; }
+                else { schedule[jobIndex] += " "; }
+            }
+            jobIndex++;
+        }
+        completedJobs++;
+
+        if(completedJobs >= jobs.size()) { break; }
+    }
+
+    printSchedule("FCFS", schedule);
+}
+
 int main(int argc, char *argv[])
 {
     if(argc > 0)
     {
         // Populate jobs vector with contents from input file
         populateJobs(argv[1]);
+
+        fcfs();
+        //spn();
+        //hrrn();
     }
 }
